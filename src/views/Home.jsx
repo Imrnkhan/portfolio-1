50f71394e252a5c871577916327461be69ff6e1b
Imrnkhan <imrankhan7581@gmail.com>
2022-07-16 13:39:48 +0530

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -91,7 +91,7 @@ const Home = () => {
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
                 <Link onClick={() => {
-                  const url = "https://drive.google.com/drive/u/0/folders/12MlJWX2c7AlK3OhQ2nH-W63q6z8k20jk"
+                  const url = "https://drive.google.com/file/d/137kK5_nTpu7hXoaLAQYpsLLm4jpkw88X/view?usp=sharing"
                   const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
                   if (newWindow) newWindow.opener = null
 
